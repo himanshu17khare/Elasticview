da7a75216705cd1e386236130597cab32906ea69
Khare4Developer <khare4developer01@gmail.com>
2018-11-21 02:13:17 +0530

    first commit

---
+++ app/src/main/java/io/armcha/sampleapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package io.armcha.sampleapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //imageElasticView.flexibility = 8f
+        imageElasticView.isDebugPathEnabled = true
+
+        buttonElasticView.setOnClickListener {
+            startActivity(Intent(this, RecyclerViewActivity::class.java))
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBarChangeListener() {
+            override fun onProgress(progress: Int) {
+                imageElasticView.flexibility = progress / 10f + 1f
+                seekBarText.text = "Flexibility is ${imageElasticView.flexibility}f"
+            }
+        })
+        seekBar.progress = 40
+    }
+}
